350c710a4cf4dc52687b65954a21046bd1fbcd9c
Stanley Pan <stanleypan.egre@gmail.com>
2016-10-25 23:22:48 +0900

    Add delegate methods for loginButton and enable Keychain Sharing

--- FirebaseSocialLogin/ViewController.swift
+++ FirebaseSocialLogin/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = FBSDKLoginButton()
+        
+        view.addSubview(loginButton)
+        //frames are obsolete, use constraints instead
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        
+        loginButton.delegate = self
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of Facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        print("Successfully logged in with Facebook.")
     }
 }
